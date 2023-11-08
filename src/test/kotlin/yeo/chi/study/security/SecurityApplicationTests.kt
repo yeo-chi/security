11e1880e79de09659812415478699547ea0afe5c
yeo-chi <00www.black@gmail.com>
2023-11-08 11:25:58 +0900

    init

---
+++ src/test/kotlin/yeo/chi/study/security/SecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package yeo.chi.study.security
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
