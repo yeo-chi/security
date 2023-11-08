5a19162ea3899d6d96d4f3d5041af16c593ddce2
yeo-chi <00www.black@gmail.com>
2023-11-08 19:10:08 +0900

    transactional 제거

--- src/main/kotlin/yeo/chi/study/security/service/UserService.kt
+++ src/main/kotlin/yeo/chi/study/security/service/UserService.kt
@@ -5,7 +5,6 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import yeo.chi.study.security.persistent.entity.UserEntity
 import yeo.chi.study.security.persistent.repository.UserRepository
 
@@ -21,7 +20,6 @@ class UserService(
             ?: throw UsernameNotFoundException("회원을 찾을 수 없습니다.")
     }
 
-    @Transactional
     fun createUser(user: UserEntity) {
         userRepository.save(user)
     }
